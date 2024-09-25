3a41091097d108b42c8449527009c712f1a0ed71
Clément Habinshuti <haby_habbes@live.com>
2024-09-25 19:53:05 +0300

    Create basic EdmModel change analyzer

---
+++ ODataModelChangeAnalyzer/ODataModelChangeAnalyzer/IModelChange.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODataModelChangeAnalyzer;
+
+internal interface IModelChange
+{
+    public ChangeKind ChangeKind { get; }
+    /// <summary>
+    /// The target element of the change. If this is an addition or modification,
+    /// this refers to the element in the new model. If this is a deletion, this
+    /// refers to the element in the old model.
+    /// </summary>
+    public IEdmElement Target { get; }
+}
+
+internal enum ChangeKind
+{
+    Addition,
+    Deletion,
+    Modification
+}
