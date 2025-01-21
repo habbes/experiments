e6ccb4a82c8e4fa5c661550b70cb6bc2f49f6cde
Clément Habinshuti <haby_habbes@live.com>
2025-01-21 14:53:57 +0300

    Basic sample app

---
+++ DotNetPerfAnalysisIntro/SampleApi/DataReader.cs
@@ -0,0 +1,37 @@
+﻿namespace SampleApi;
+
+internal class DataReader
+{
+    public static async Task<List<LocationData>> ReadDataAsync(Stream input)
+    {
+        List<LocationData> data = new();
+        var reader = new StreamReader(input);
+
+        string content = await reader.ReadToEndAsync();
+
+        var lines = content.Split(Environment.NewLine);
+
+        foreach (var line in lines)
+        {
+            var words = line.Split(" ");
+            string name = default;
+            double temperature = default;
+            foreach (var word in words)
+            {
+                if (word.StartsWith("location:"))
+                {
+                    name = word.Split(':')[1];
+                }
+                else if (word.StartsWith("temp:"))
+                {
+                    temperature = double.Parse(word.Split(':')[1]);
+                }
+            }
+
+            var locationData = new LocationData(name!, temperature);
+            data.Add(locationData);
+        }
+
+        return data;
+    }
+}
---
+++ DotNetPerfAnalysisIntro/SampleApi/Models.cs
@@ -0,0 +1,5 @@
+﻿namespace SampleApi;
+
+record LocationData(string Name, double Temperature);
+
+record LocationStats(string Name, double MaxTemperature, double MinTemperature, double MeanTemperature);
